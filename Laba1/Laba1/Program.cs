9999093db3c55679d17187b66ea69590cc6c2419
MEAWWS <115928512+MEAWWS@users.noreply.github.com>
2023-02-17 11:26:10 +0300

    Add files via upload

---
+++ Laba1/Laba1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using labaLibrary1;
+
+namespace Laba1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LanguageLibrary language = new LanguageLibrary();
+            Queue<string> patient = new Queue<string>();
+            Console.WriteLine("Выберите язык/Select language: 1 - Русский, 2 - English");
+            int selectln = int.Parse(Console.ReadLine());
+            while(true)
+            {
+                Console,WriteLine(language.Language(selectln, 1));
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine(language.Language(selectln, 2));
+                        patient.Enqueue(Console.ReadLine());
+                        break;
+                    case 2:
+                        if
+                }
+            }
+        }
+    }
+}
---
+++ labaLibrary1/labaLibrary1/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labaLibrary1
+{
+    internal class LanguageLibrary
+    {
+
+        public Dictionary<int, string> Russian = new Dictionary<int, string>()
+        {
+            { 1, "Введите: 1 - для добавления в очередь; 2 - для принятия на приём; 3 - Показать очередь" },
+            { 2, "Введиет ФИО:" },
+            { 3, "Принять на приём"},
+            { 4, "Нет пациентов"}
+
+        };
+        public Dictionary<int, string> English = new Dictionary<int, string>()
+        {
+            {1, "Enter: 1 - to add to the queue, 2 - for admission, 3 - show queue" },
+            {2, "Enter your full name:" },
+            {3, "Accept for an appointment" },
+            {4, "No patients" }
+
+        };
+
+        public string Language(int ln,int key)
+        {
+            if (ln == 1)
+            {
+                Russian.TryGetValue(key, out string s);
+                return s;
+            }
+            else
+            {
+                English.TryGetValue(key, out string es);
+                return es;
+                    
+            }
+        }
+    }
+}
+
+   
